75a645bc6791eaa2d2deb1e3bb300eb0600faaed
santhosh madur <santhosh.madur@vation.com>
2025-01-22 02:25:06 +0530

    updated headernavigation component

---
+++ core/src/main/java/com/venia/core/models/commerce/HeaderNavigationModel.java
@@ -0,0 +1,102 @@
+package com.venia.core.models.commerce;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import java.util.List;
+
+@Model(
+        adaptables = SlingHttpServletRequest.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class HeaderNavigationModel {
+
+    @ValueMapValue
+    private String headerLogo;
+
+    @ValueMapValue
+    private String headerLogoLink;
+
+    @ValueMapValue
+    private Boolean linkTarget;
+
+    @ChildResource(name = "navLinks")
+    private List<NavLinksModel> navLinks;
+
+    @ValueMapValue
+    private String searchText;
+
+    @ValueMapValue
+    private String searchIcon;
+
+    @ValueMapValue
+    private String addToBagText;
+
+    @ValueMapValue
+    private String addToBagIcon;
+
+    public String getHeaderLogo() {
+        return headerLogo;
+    }
+
+    public String getHeaderLogoLink() {
+        return headerLogoLink;
+    }
+
+    public Boolean getLinkTarget() {
+        return linkTarget;
+    }
+
+    public List<NavLinksModel> getNavLinks() {
+        return navLinks;
+    }
+
+    public String getSearchText() {
+        return searchText;
+    }
+
+    public String getSearchIcon() {
+        return searchIcon;
+    }
+
+    public String getAddToBagText() {
+        return addToBagText;
+    }
+
+    public String getAddToBagIcon() {
+        return addToBagIcon;
+    }
+
+    @Model(
+            adaptables = Resource.class,
+            defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+    )
+    public static class NavLinksModel {
+
+        @ValueMapValue
+        private String navTitle;
+
+        @ValueMapValue
+        private String navLink;
+
+        @ValueMapValue
+        private String linkTarget;
+
+        public String getNavTitle() {
+            return navTitle;
+        }
+
+        public String getNavLink() {
+            return navLink;
+        }
+
+        public String getLinkTarget() {
+            return linkTarget;
+        }
+    }
+}
+
